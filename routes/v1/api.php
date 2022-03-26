d14938f67f105b78ca372ab011595b7ddbab146c
Shaon Majumder <smazoomder@gmail.com>
2022-03-26 17:10:58 +0600

    get gmt client time of visiting website

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -12,21 +12,5 @@ Route::middleware(["auth:sanctum"])->group(function () {
      * Logout Authenticated User
      */
     Route::any('/logout', [LoginController::class, "logout"]);
-    
     Route::post('/visited', [ApiController::class, "visited"]);
-    
-    /**
-     * Driver Profile / Details
-     */
-    Route::prefix('driver')->group(function(){
-        Route::get('/profile', [DriverController::class, "getProfile"]);
-        
-        Route::prefix('parcels')->group(function(){
-            Route::get('/list/{status}', [DriverController::class, "listParcel"]);
-            Route::get('/summary', [DriverController::class, "getParcelsSummary"]);
-            Route::post('/change-status', [DriverController::class, "changeParcelStatus"]);    
-        });
-    });
-    
-
 });
